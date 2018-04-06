2295e63fe2e6971273e57bc8bde8c816d15235e0
David Roncancio <david.roncancio@gmail.com>
2018-04-06 08:50:54 -0500

    totalmem instead of freemem

--- lab4/exercise2/index.js
+++ lab4/exercise2/index.js
@@ -1,10 +1,17 @@
 
-const { cpus, freemem } = require('os');
+const { cpus, totalmem } = require('os');
 const prettyBytes = require('pretty-bytes');
 
 function checkSystem() {
     console.log('Checking your system');
-    console.log(cpus(), prettyBytes(freemem()));
+    if (totalmem() < 2e9) {
+        return console.log('This app needs at least 2GB of RAM');
+    }
+    if (cpus().length < 2) {
+        return console.log('Processor is not supported');
+    }
+    console.log('System is checked succesfully');
+    // console.log(cpus(), prettyBytes(totalmem()));
 }
 
 checkSystem();
